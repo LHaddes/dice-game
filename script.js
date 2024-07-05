6669ec6a73ed71ac138ed64e03d3f90bd95ba17e
Kilian <haddesgaming@gmail.com>
2024-07-05 12:45:12 +0200

    Rename files + function instead of refresh for dice roll

---
+++ script.js
@@ -0,0 +1,26 @@
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var diceImage1 = "dice" + randomNumber1 + ".png";
+  var srcImage1 = "./images/" + diceImage1;
+
+  document.querySelector(".img1").setAttribute("src", srcImage1);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var diceImage2 = "dice" + randomNumber2 + ".png";
+  var srcImage2 = "./images/" + diceImage2;
+
+  document.querySelector(".img2").setAttribute("src", srcImage2);
+
+  if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+  }
+
+  if(randomNumber1 === randomNumber2)
+  {
+    document.querySelector("h1").innerHTML = "It's a draw !";
+  }
+
+}
